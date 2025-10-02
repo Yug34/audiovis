1f36232e6e00fa6f0f5d841b644ac07e8e4fce99
Yug34 <yug.gjr@gmail.com>
2025-10-02 14:29:47 +0530

    remove dead code

--- src/components/AudioControls/index.ts
+++ src/components/AudioControls/index.ts
@@ -1,4 +1,3 @@
 export { AudioCanvas } from './AudioCanvas';
 export { AudioUpload } from './AudioUpload';
-export { GainControls } from './GainControls';
 export { ControlsPanel } from './ControlsPanel';
