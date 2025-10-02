45b6f5967e63188d4a717e8f5c5fde698e108c2e
Yug34 <yug.gjr@gmail.com>
2025-10-02 11:43:34 +0530

    shorten nevergonnagiveyouup

--- src/lib/audio/constants.ts
+++ src/lib/audio/constants.ts
@@ -3,7 +3,7 @@ import type { CircularVisualizationParams } from '@/types/audio';
 export const DEFAULT_VISUALIZATION_PARAMS: CircularVisualizationParams = {
   stepFactor: 1.004,
   colorStepFactor: 100,
-  opacity: 0.985,
+  opacity: 1,
   radius: 2,
   panSpeed: 0.004,
   panRadius: 0.03,
